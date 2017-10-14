82fedb2aa1019075e7aa6246192d06e53b5985bd
pidoubleyou <pidoubleyou@arcor.de>
2017-10-14 10:19:33 +0200

    First strategy

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -5,12 +5,12 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Default C# folding player";
+		public static readonly string VERSION = "First simple strategy";
 
 		public static int BetRequest(JObject gameState)
 		{
             //TODO: Use this method to return the value You want to bet
-            return 0;
+            return 1000;
 		}
 
 		public static void ShowDown(JObject gameState)
