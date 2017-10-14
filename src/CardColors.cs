c9f5fa82bdce437b587b88c64905df5524b00662
eliExplorer <betty_weiss2001@yahoo.de>
2017-10-14 10:54:17 +0200

    Consts für Cards
    Consts für Cards hinzugefügt

---
+++ src/CardColors.cs
@@ -0,0 +1,11 @@
+﻿namespace Nancy.Simple
+{
+    public enum CardColors
+    {
+
+    internal static string clubs = "clubs";
+    internal static string spades = "spades";
+    internal static string hearts = "hearts";
+    internal static string diamonds = "diamonds";
+}
+}
