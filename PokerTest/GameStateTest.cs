8ca505244c23498fba4f9821ea0a32a454368342
pidoubleyou <pidoubleyou@arcor.de>
2017-10-14 11:10:22 +0200

    new configuration Local

---
+++ PokerTest/GameStateTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy.Simple;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerTest
+{
+    [TestClass]
+    public class GameStateTest
+    {
+
+        [TestMethod]
+        public void SelfTest()
+        {
+            var gameState = new GameState()
+            {
+                Players = new PlayerInfo[]
+                {
+                    
+                }
+            };
+        }
+    }
+}
