6211961937fc696724ba5a9435adea0adaf7ae82
haeusner <32773318+haeusner@users.noreply.github.com>
2017-10-14 10:01:22 +0200

    Version angepasst

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -4,7 +4,7 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Default C# folding player";
+		public static readonly string VERSION = "Playing from Dusk till Dawn";
 
 		public static int BetRequest(JObject gameState)
 		{
