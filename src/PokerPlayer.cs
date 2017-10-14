713259c29f0f16cd7be17f328fd8741fb5299ef4
pidoubleyou <pidoubleyou@arcor.de>
2017-10-14 11:24:18 +0200

    Fold erweitert

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -7,10 +7,20 @@ namespace Nancy.Simple
 	{
 		public static readonly string VERSION = "Playing from Dusk till Dawn";
 
-		public static int BetRequest(JObject gameState)
+		public static int BetRequest(JObject gameStateJson)
 		{
-            //TODO: Use this method to return the value You want to bet
-            return 1000;
+            var deserializer = new Deserializer();
+            var gameState = deserializer.Deserialize(gameStateJson);
+
+            var eval = new Evaluator();
+            var state = eval.GetScore(gameState.Self.Cards);
+            switch(state)
+            {
+                case State.Fold:
+                    return 0;
+                default:
+                    return 1000;
+            }
 		}
 
 		public static void ShowDown(JObject gameState)
