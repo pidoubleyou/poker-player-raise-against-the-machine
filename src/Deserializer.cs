61b538871e21c372ef15de15dca415b105a2af49
pidoubleyou <pidoubleyou@arcor.de>
2017-10-14 10:12:08 +0200

    Testprojekt und Datenstrukturen

---
+++ src/Deserializer.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple
+{
+    public class Deserializer
+    {
+        public void Deserialize(JObject gameStateJson)
+        {
+
+        }
+    }
+}
